3c9b57c99c0194cde68a716d88688a82fe271af2
Bennett Wong <bennettryuma@hotmail.com>
2019-04-21 20:26:19 -0700

    Basic item interaction

---
+++ Mom's Home/Assets/Scripts/IneractionObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IneractionObject : MonoBehaviour
+{
+    public void DoInteraction()
+    {
+        //Picked up and put in inventory
+        gameObject.SetActive(false);
+    }
+}
