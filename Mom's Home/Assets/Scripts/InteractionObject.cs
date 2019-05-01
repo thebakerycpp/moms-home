c60cf88e3aad242249d11102ec6bdc7f4c4d9032
Bennett Wong <bennettryuma@hotmail.com>
2019-05-01 09:44:39 -0700

    Added item interaction

---
+++ Mom's Home/Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    public void DoInteraction()
+    {
+        //Picked up and put in inventory
+        gameObject.SetActive(false);
+    }
+}
